63d1d0a7dd2b216263165fd09f5a0106073d038f
Chadhendrixs <bryantmoyle@gmail.com>
2025-07-26 19:43:25 -0400

    w

--- backend/main.py
+++ backend/main.py
@@ -173,6 +173,6 @@ def startup_event():
 def read_root():
     return {"message": "Hello from FastAPI!"}
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port)
+#if __name__ == "__main__":
+#    port = int(os.environ.get("PORT", 8000))
+#    uvicorn.run("main:app", host="0.0.0.0", port=port)
