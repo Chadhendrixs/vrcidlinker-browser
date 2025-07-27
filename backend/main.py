d892268f571fe32e48847d0210994adb9f7915ad
Chadhendrixs <bryantmoyle@gmail.com>
2025-07-26 21:00:55 -0400

    Fixed origins for new url

--- backend/main.py
+++ backend/main.py
@@ -41,6 +41,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://vrcidlinker-browser-frontend.onrender.com"
+        "https://vrclinked.com",
         ],
     allow_credentials=True,
     allow_methods=["*"],
